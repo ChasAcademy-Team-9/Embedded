a51cf352c44006d97a30012d98f313a4f9449651
Jessica Sjösten <jessica.e.sjosten@gmail.com>
2025-09-09 09:37:40 +0200

    addad secrets to .gitignore and changed from arduinosecrets to espsecrets

--- Chas Advance Arduino/include/ARDUINOSECRETS.h
+++ Chas Advance Arduino/include/ARDUINOSECRETS.h
@@ -1,8 +1,13 @@
 #ifndef ARDUINOSECRETS_H
 #define ARDUINOSECRETS_H
 
-const char *ssid = "Chas Academy";
-const char *password = "EverythingLouderThanEverythingElse";
+// const char *ssid = "Chas Academy";
+// const char *password = "EverythingLouderThanEverythingElse";
+// const char *host = "10.100.3.222";
+// const int port = 80;
+
+const char *ssid = "TP-Link_2928_5G";
+const char *password = "4d3c2b1a";
 const char *host = "10.100.3.222";
 const int port = 80;
 
--- Chas Advance ESP32/include/ARDUINOSECRETS.h
+++
@@ -1,6 +0,0 @@
-#ifndef ARDUINOSECRETS_H
-#define ARDUINOSECRETS_H
-
-const char *ssid = "Chas Academy";
-const char *password = "EverythingLouderThanEverythingElse";
-#endif
