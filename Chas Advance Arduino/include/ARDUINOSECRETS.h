4410ea7b5594cb84cd2b54e4b52409b2fdd26580
Jessica Sjösten <jessica.e.sjosten@gmail.com>
2025-09-19 11:01:27 +0200

    Remove ARDUINOSECRETS.h from tracking

--- Chas Advance Arduino/include/ARDUINOSECRETS.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARDUINOSECRETS_H
-#define ARDUINOSECRETS_H
-
-const char *ssid = "ESP32 AccessPoint";
-const char *password = "123456789";
-const char *host = "192.168.4.1"; // ESP32 IP
-const int port = 80;
-
-#endif // ARDUINOSECRETS_H
