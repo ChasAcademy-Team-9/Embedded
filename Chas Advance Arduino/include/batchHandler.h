d1c6d774ffe695aac921c5744e077244117ca210
DavidJanReyes <davidjanreyes@gmail.com>
2025-09-26 10:00:54 +0200

    Added batch send retry & error logging

--- Chas Advance Arduino/include/batchHandler.h
+++ Chas Advance Arduino/include/batchHandler.h
@@ -5,6 +5,11 @@
 #include <vector>
 #include <Arduino.h>
 
+struct FailedBatch {
+    String json;
+    uint8_t retries;
+};
+
 void batchSensorReadings(const SensorData &data);
 SensorData calculateMedian(std::vector<SensorData>& buffer);
 std::vector<SensorData>& getBatchBuffer();
